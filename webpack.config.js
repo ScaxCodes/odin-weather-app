4ea07b8d1e855cfef0eb8a9fc83d43565cd99eaa
Einschleicher <116537717+Einschleicher@users.noreply.github.com>
2023-10-30 13:33:54 +0100

    Add basic boilerplate and npm setup

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+/* eslint-disable no-undef */
+const path = require("path");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.js",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  devtool: "source-map",
+  module: {
+    rules: [
+      {
+        test: /\.(?:js|mjs|cjs)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: [["@babel/preset-env", { targets: "defaults" }]],
+          },
+        },
+      },
+    ],
+  },
+};
