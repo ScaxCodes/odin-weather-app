3f9d79b0dbfb9ae08b953fb173ac19b1dab63599
Einschleicher <116537717+Einschleicher@users.noreply.github.com>
2023-11-05 05:14:02 +0100

    Add function to fetch weather data

--- dist/bundle.js
+++ dist/bundle.js
@@ -3,7 +3,35 @@ var __webpack_exports__ = {};
 /*!**********************!*\
   !*** ./src/index.js ***!
   \**********************/
-console.log("Hello");
+async function fetchWeather(location) {
+  try {
+    const API_KEY = "6d61c6b48aad4c6a9d0195337232810";
+    const response = await fetch(`https://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=${location}`, {
+      mode: "cors"
+    });
+    const weatherJson = await response.json();
+    return weatherJson;
+  } catch (error) {
+    console.error(`Error: ${error.message}`);
+  }
+}
+
+// async function printWeather() {
+//   const weather = await fetchWeather("aschaffenburg");
+//   console.log("This is the current weather in Aschaffenburg:");
+//   console.log(weather);
+// }
+
+async function processWeather(location) {
+  const weather = await fetchWeather(location);
+  const weatherSelectedData = {};
+  weatherSelectedData.name = weather.location.name;
+  weatherSelectedData.celsius = weather.current.temp_c;
+  weatherSelectedData.text = weather.current.condition.text;
+  weatherSelectedData.icon = weather.current.condition.icon;
+  console.log(weatherSelectedData);
+}
+processWeather("mainz");
 /******/ })()
 ;
 //# sourceMappingURL=bundle.js.map
