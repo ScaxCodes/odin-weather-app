362e0d4b98c46f7be58622d45cc283f2d454dc3a
Einschleicher <116537717+Einschleicher@users.noreply.github.com>
2023-11-06 09:57:12 +0100

    Add functionality to fetch forecast data for 2 more days

--- dist/bundle.js
+++ dist/bundle.js
@@ -6,30 +6,43 @@ var __webpack_exports__ = {};
 async function fetchWeather(location) {
   try {
     const API_KEY = "6d61c6b48aad4c6a9d0195337232810";
-    const response = await fetch(`https://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=${location}`, {
+    const response = await fetch(`https://api.weatherapi.com/v1/forecast.json?key=${API_KEY}&q=${location}&days=3`, {
       mode: "cors"
     });
     const weatherJson = await response.json();
+    console.log(weatherJson);
     return weatherJson;
   } catch (error) {
     console.error(`Error: ${error.message}`);
   }
 }
-
-// async function printWeather() {
-//   const weather = await fetchWeather("aschaffenburg");
-//   console.log("This is the current weather in Aschaffenburg:");
-//   console.log(weather);
-// }
-
 async function processWeather(location) {
   const weather = await fetchWeather(location);
-  const weatherSelectedData = {};
-  weatherSelectedData.name = weather.location.name;
-  weatherSelectedData.celsius = weather.current.temp_c;
-  weatherSelectedData.fahrenheit = weather.current.temp_f;
-  weatherSelectedData.text = weather.current.condition.text;
-  weatherSelectedData.icon = weather.current.condition.icon;
+  const weatherSelectedData = {
+    current: {},
+    forecastDay2: {},
+    forecastDay3: {}
+  };
+  weatherSelectedData.current.name = weather.location.name;
+  weatherSelectedData.current.celsius = weather.current.temp_c;
+  weatherSelectedData.current.fahrenheit = weather.current.temp_f;
+  weatherSelectedData.current.text = weather.current.condition.text;
+  weatherSelectedData.current.icon = weather.current.condition.icon;
+  weatherSelectedData.forecastDay2.date = weather.forecast.forecastday[1].date;
+  weatherSelectedData.forecastDay2.maxtemp_c = weather.forecast.forecastday[1].day.maxtemp_c;
+  weatherSelectedData.forecastDay2.mintemp_c = weather.forecast.forecastday[1].day.mintemp_c;
+  weatherSelectedData.forecastDay2.maxtemp_f = weather.forecast.forecastday[1].day.maxtemp_f;
+  weatherSelectedData.forecastDay2.mintemp_f = weather.forecast.forecastday[1].day.mintemp_f;
+  weatherSelectedData.forecastDay2.text = weather.forecast.forecastday[1].day.condition.text;
+  weatherSelectedData.forecastDay2.icon = weather.forecast.forecastday[1].day.condition.icon;
+  weatherSelectedData.forecastDay3.date = weather.forecast.forecastday[2].date;
+  weatherSelectedData.forecastDay3.maxtemp_c = weather.forecast.forecastday[2].day.maxtemp_c;
+  weatherSelectedData.forecastDay3.mintemp_c = weather.forecast.forecastday[2].day.mintemp_c;
+  weatherSelectedData.forecastDay3.maxtemp_f = weather.forecast.forecastday[2].day.maxtemp_f;
+  weatherSelectedData.forecastDay3.mintemp_f = weather.forecast.forecastday[2].day.mintemp_f;
+  weatherSelectedData.forecastDay3.text = weather.forecast.forecastday[2].day.condition.text;
+  weatherSelectedData.forecastDay3.icon = weather.forecast.forecastday[2].day.condition.icon;
+  console.log(weatherSelectedData);
   return weatherSelectedData;
   // console.log(weatherSelectedData);
 }
@@ -42,15 +55,23 @@ searchButton.addEventListener("click", () => {
 });
 async function displayWeather(location) {
   const weather = await processWeather(location);
+
+  // Select current DOM
   const city = document.querySelector(".city");
   const temp = document.querySelector(".temp");
   const text = document.querySelector(".text");
   const icon = document.querySelector(".icon");
-  city.textContent = weather.name;
-  temp.textContent = `${weather.celsius}°`;
-  text.textContent = weather.text;
-  icon.innerHTML = `<img src="${weather.icon}">`;
+
+  // Fill current DOM
+  city.textContent = weather.current.name;
+  temp.textContent = `${weather.current.celsius}°`;
+  text.textContent = weather.current.text;
+  icon.innerHTML = `<img src="${weather.current.icon}">`;
+
+  // Select forecast DOM
 }
+
+processWeather("mainz");
 /******/ })()
 ;
 //# sourceMappingURL=bundle.js.map
