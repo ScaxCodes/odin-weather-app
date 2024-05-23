9e51e96df6e496f6ad0d5e32a389965df44accc0
ScaxCodes <116537717+ScaxCodes@users.noreply.github.com>
2024-05-23 20:44:37 +0200

    Unify arguments in renderfunctions

--- src/index.js
+++ src/index.js
@@ -22,8 +22,8 @@ let unitIsCelsius = true;
  - done 
 8. refactor code with new parsed API data
  - done
-
 9. Make textsearch working again or delete feature
+10. Enhance styling
 
 */
 
@@ -36,7 +36,6 @@ async function positionSuccess({ coords }) {
   const weather = await fetchWeather("", coords.latitude, coords.longitude);
   modal.style.display = "none";
   renderWeather(weather);
-  // displayWeather("", coords.latitude, coords.longitude);
 }
 
 function positionError() {
@@ -80,15 +79,10 @@ async function fetchWeather(location, lat, lon) {
   try {
     const API_KEY = "6d61c6b48aad4c6a9d0195337232810";
     const response = await fetch(
-      // `https://api.weatherapi.com/v1/forecast.json?key=${API_KEY}&q=${location}&days=3`,
       `https://api.weatherapi.com/v1/forecast.json?key=${API_KEY}&q=${lat},${lon}&days=3`,
       { mode: "cors" }
     );
     const weather = await response.json();
-    // console.log("weather ", weather);
-    // console.log("parsedCurrentWeather", parseCurrentWeather(weather));
-    // console.log("parsedForecastWeather", parseForecastWeather(weather));
-    // console.log("parsedHourlyWeather", parseHourlyWeather(weather));
     return weather;
   } catch (error) {
     console.error(`Error: ${error.message}`);
@@ -152,7 +146,7 @@ function parseHourlyWeather({
 function renderWeather(weather) {
   renderCurrentWeather(weather);
   renderForecastWeather(weather);
-  renderHourlyWeather(parseHourlyWeather(weather));
+  renderHourlyWeather(weather);
 }
 
 function renderCurrentWeather(weather) {
@@ -188,11 +182,13 @@ function renderForecastWeather(weather) {
   });
 }
 
-function renderHourlyWeather(hourly) {
+function renderHourlyWeather(weather) {
+  const hourlyWeather = parseHourlyWeather(weather);
+
   const hourlySection = document.querySelector("[data-hour-section]");
   const hourRowTemplate = document.getElementById("hour-row-template");
   hourlySection.innerHTML = "";
-  hourly.forEach((hour) => {
+  hourlyWeather.forEach((hour) => {
     const element = hourRowTemplate.content.cloneNode(true);
     setValue("temp", hour.temp_c, { parent: element });
     setValue("fl-temp", hour.feelslike_c, { parent: element });
@@ -203,60 +199,6 @@ function renderHourlyWeather(hourly) {
     element.querySelector("[data-icon]").src = "https:" + hour.icon;
     hourlySection.append(element);
   });
-}
-
-async function processWeather(location, lat, lon) {
-  const weather = await fetchWeather(location, lat, lon);
-  if (weather.error) {
-    console.log("ERROR OCCURRED");
-    return 1;
-  }
-  const weatherSelectedData = {
-    current: {},
-    forecastDay2: {},
-    forecastDay3: {},
-    hourly: parseHourlyWeather(weather),
-  };
-
-  // Process current weather
-  weatherSelectedData.current.name = weather.location.name;
-  weatherSelectedData.current.celsius = weather.current.temp_c;
-  weatherSelectedData.current.fahrenheit = weather.current.temp_f;
-  weatherSelectedData.current.text = weather.current.condition.text;
-  weatherSelectedData.current.icon = weather.current.condition.icon;
-
-  // Process weather for tomorrow
-  weatherSelectedData.forecastDay2.date = weather.forecast.forecastday[1].date;
-  weatherSelectedData.forecastDay2.maxtemp_c =
-    weather.forecast.forecastday[1].day.maxtemp_c;
-  weatherSelectedData.forecastDay2.mintemp_c =
-    weather.forecast.forecastday[1].day.mintemp_c;
-  weatherSelectedData.forecastDay2.maxtemp_f =
-    weather.forecast.forecastday[1].day.maxtemp_f;
-  weatherSelectedData.forecastDay2.mintemp_f =
-    weather.forecast.forecastday[1].day.mintemp_f;
-  weatherSelectedData.forecastDay2.text =
-    weather.forecast.forecastday[1].day.condition.text;
-  weatherSelectedData.forecastDay2.icon =
-    weather.forecast.forecastday[1].day.condition.icon;
-
-  // Process weather for the day after tomorrow
-  weatherSelectedData.forecastDay3.date = weather.forecast.forecastday[2].date;
-  weatherSelectedData.forecastDay3.maxtemp_c =
-    weather.forecast.forecastday[2].day.maxtemp_c;
-  weatherSelectedData.forecastDay3.mintemp_c =
-    weather.forecast.forecastday[2].day.mintemp_c;
-  weatherSelectedData.forecastDay3.maxtemp_f =
-    weather.forecast.forecastday[2].day.maxtemp_f;
-  weatherSelectedData.forecastDay3.mintemp_f =
-    weather.forecast.forecastday[2].day.mintemp_f;
-  weatherSelectedData.forecastDay3.text =
-    weather.forecast.forecastday[2].day.condition.text;
-  weatherSelectedData.forecastDay3.icon =
-    weather.forecast.forecastday[2].day.condition.icon;
-
-  // console.log("weatherSeletedData ", weatherSelectedData);
-  return weatherSelectedData;
 }
 
 const searchButton = document.querySelector("#search");
@@ -272,87 +214,6 @@ toggleButton.addEventListener("click", () => {
   temptoggle();
 });
 
-async function displayWeather(location, lat, lon) {
-  const currentDiv = document.querySelector(".current-container");
-  const forecastDivs = document.querySelectorAll(".forecast");
-  const elementGIF = document.querySelector(".gif");
-  currentDiv.style.display = "none";
-  forecastDivs.forEach((div) => {
-    div.style.display = "none";
-  });
-  elementGIF.style.display = "none";
-
-  modal.style.display = "flex";
-
-  const weather = await processWeather(location, lat, lon);
-
-  if (weather === 1) {
-    console.log("ERROR OCCURRED");
-    inputField.value = "";
-    return 1;
-  }
-
-  modal.style.display = "none";
-
-  currentDiv.style.display = "flex";
-  forecastDivs.forEach((div) => {
-    div.style.display = "flex";
-  });
-  elementGIF.style.display = "flex";
-  inputField.value = "";
-
-  // Select current DOM
-  const city = document.querySelector(".city");
-  const temp = document.querySelector(".temp");
-  const tempF = document.querySelector(".temp-f");
-  const text = document.querySelector(".text");
-  const icon = document.querySelector(".icon");
-
-  // Fill current DOM
-  city.textContent = weather.current.name;
-  temp.textContent = `${weather.current.celsius}°`;
-  tempF.textContent = `${weather.current.fahrenheit}°`;
-  text.textContent = weather.current.text;
-  icon.innerHTML = `<img src="${weather.current.icon}">`;
-
-  // Select forecast DOM
-  const dateForecast = document.querySelectorAll(".forecast .date");
-  const tempForecast = document.querySelectorAll(".forecast .temp");
-  const tempFForecast = document.querySelectorAll(".forecast .temp-f");
-  const textForecast = document.querySelectorAll(".forecast .text");
-  const iconForecast = document.querySelectorAll(".forecast .icon");
-
-  // Fill forecast DOM
-  dateForecast.forEach((div, index) => {
-    div.textContent = weather[`forecastDay${index + 2}`].date;
-  });
-
-  tempForecast.forEach((div, index) => {
-    div.textContent = `
-    ${weather[`forecastDay${index + 2}`].mintemp_c}° - 
-    ${weather[`forecastDay${index + 2}`].maxtemp_c}°
-    `;
-  });
-
-  tempFForecast.forEach((div, index) => {
-    div.textContent = `
-    ${weather[`forecastDay${index + 2}`].mintemp_f}° - 
-    ${weather[`forecastDay${index + 2}`].maxtemp_f}°
-    `;
-  });
-
-  textForecast.forEach((div, index) => {
-    div.textContent = weather[`forecastDay${index + 2}`].text;
-  });
-
-  iconForecast.forEach((div, index) => {
-    div.innerHTML = `<img src="${weather[`forecastDay${index + 2}`].icon}">`;
-  });
-
-  // Display GIF
-  // displayGIF(weather.current.text);
-}
-
 function setValue(selector, value, { parent = document } = {}) {
   parent.querySelector(`[data-${selector}]`).textContent = value;
 }
@@ -363,9 +224,4 @@ function formatDay(timestamp) {
 
 function formatTime(timestamp) {
   return format(new Date(timestamp * 1000), "ha");
-}
-
-function getIconUrl(icon, { large = false } = {}) {
-  const size = large ? "@2x" : "";
-  return `http://openweathermap.org/img/wn/${icon}${size}.png`;
 }
